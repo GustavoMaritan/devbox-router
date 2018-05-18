4a698201339931390df49cd6843ad2335fb3f2a0
Gustavo_EngSolutions <gustavo@engsolutions.com.br>
2018-05-18 11:38:48 -0300

    master - controller

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,36 @@
-import { Application } from "express";
-
 declare interface Controller {
-    static prefix(flags: string)
-    static prefix(flags: string, description?: string)
-    static prefix(flags: string, description?: string, mussak?: object)
 
-    /**
-     * 
-     * @param params tesatfsdjhkjd
-     */
-    private static post(...params)
+    static post(action: Function);
+    static post(uri: String, action: Function);
+    static post(uri: String, prefix: boolean, action: Function);
+    static post(uri: String, options: {
+        prefix?: boolean
+    }, action: Function);
 }
 
 class Router {
 
-    static init(app: Application, options: {
-        prefix?: String,
-        folder?: String,
-        fileName?: String = 'controller.js',
+    static init(app: express, options: {
+        prefix?: string,
+        folder?: 'src/core',
+        fileName?: 'controller.js',
         actionFilter?: Function
     }): Controller
 
+    /**
+     * @description
+     *  -
+     *  - Cria padrão de rota para o controller
+     *  - Cria objeto para registrar rotas
+     * 
+     * @param name 
+     *  [controller_name] OR [__filename]
+     * 
+     * @example
+     * 
+     * require('devbox-router').controller('usuario');
+     * require('devbox-router').controller(__filename);
+     */
     static controller(name: string): Controller
 }
 
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const find = require('devbox-recursive-find');
-const Router = require('./lib/router');
+const Controller = require('./lib/router');
 
 class Router {
 
     static init(app, options) {
-        if (options.prefix) Router._prefix(options.prefix);
+        if (options.prefix) Controller._prefix(options.prefix);
 
         const files = find.file(options.folder || './src/core', options.fileName || 'controller.js');
         files.forEach(x => require(path.resolve(x)));
 
-        for (let i in Router.routes)
-            Router.routes[i].forEach(x => {
+        for (let i in Controller.routes)
+            Controller.routes[i].forEach(x => {
                 app[x.method](x.uri, options.actionFilter
                     ? options.actionFilter(x.action, x.options)
                     : async (req, res) => { await x.action(req, res) }
                 );
             });
 
-        return Router;
+        return Controller;
     }
 
     static controller(name) {
-        Router._set(name);
-        return Router;
+        Controller._set(name);
+        return Controller;
     }
 }
 
