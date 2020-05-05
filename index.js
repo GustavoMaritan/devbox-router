aa25b31583e5bd3715700dd36f02d500ab633d71
Gustavo <gustavomaritan@gmail.com>
2020-05-05 12:55:12 -0300

    Validar Conflitos rotas e tratar erros controller

--- index.js
+++ index.js
@@ -43,14 +43,16 @@ class Router {
                 case 'get': return 'green';
                 case 'post': return 'yellow';
                 case 'put': return 'blue';
-                case 'delete': return 'red';
+                case 'delete': return 'magenta';
             }
         }
+
         if (print_route) console.log(colors.gray('__________________________________________________________________'));
         if (print_route) console.log(colors.magenta('> Rotas'));
 
         for (let i in Controller.routes) {
             if (print_route) console.log(`\t Controller: ${colors.green(i)}`);
+
             Controller.routes[i].forEach(x => {
 
                 if (print_route) {
@@ -68,8 +70,35 @@ class Router {
                     app[x.method](x.uri, options.actionFilter(x.action, x.options));
                 }
             });
+
+            if (print_route && Controller._errorRoutes[i]) {
+                console.log(`\t${colors.yellow(`* Rotas não registradas(${colors.red('CONFLITO')})`)}`);
+                Controller._errorRoutes[i].rotas.forEach(y => {
+                    console.log('\t',
+                        `${colors[methodColor(y.item.method)](_padRight(y.item.method))}  -  ${colors.red(y.item.url)} | ` +
+                        colors.grey(`Controller: ${y.conflito.ctrl} | ${y.conflito.method} - ${y.conflito.url}`)
+                    );
+                });
+            }
+            if (print_route) console.log('');
         }
         if (print_route) console.log(colors.gray('__________________________________________________________________'));
+        if (!print_route && !!Controller._errorRoutes) {
+            console.log(colors.red('> Erro ao registrar rotas'));
+            Object.keys(Controller._errorRoutes).forEach(x => {
+                console.log(`\t Controller: ${colors.green(x)}`);
+                Controller._errorRoutes[x].rotas.forEach(y => {
+                    const message =
+                        colors.red('\t\t> Rota') +
+                        colors.gray(`\t\t\t| Ctrl: ${y.item.ctrl}, Url: ${y.item.url}, Method: ${y.item.method} \n`) +
+                        colors.magenta('\t\t> Conflito com') +
+                        colors.gray(`\t\t| Ctrl: ${y.conflito.ctrl}, Url: ${y.conflito.url}, Method: ${y.conflito.method} \n`) +
+                        colors.yellow('\t\t----------------------------------------------------------------------------');
+                    console.log(message);
+                });
+                console.log(``);
+            });
+        }
         return Controller;
     }
 
